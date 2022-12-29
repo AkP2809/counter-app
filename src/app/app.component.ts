6cefbcbd3230516f2344def842571387d6c2642b
Armaan Khan <48325615+AkP2809@users.noreply.github.com>
2022-12-29 10:37:52 +0530

    Master commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'counter-app';
+  title = 'Counter App';
+
+  counterValue : number = 0;
+
+  resetCounter = () => {
+    this.counterValue = 0;
+  };
+  
+  incrementCounter = () => {
+    this.counterValue = this.counterValue + 1;
+  };
+  
+  decrementCounter = () => {
+    if(this.counterValue == 0)
+      return;
+
+    this.counterValue = this.counterValue - 1;
+  };
 }
